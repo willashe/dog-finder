262da5d8601ad6731cea1c3ac552464ec05e0790
Will Ashe <willashe@hotmail.com>
2019-04-11 20:57:28 -0500

    hot module replacement setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './components/App';
+
 ReactDOM.render(
-  <h1>DogFinder!</h1>,
+  <App />,
   document.getElementById('root')
 );
+
+module.hot.accept();
